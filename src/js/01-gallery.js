8fa9fb1a7088b089c1aeb7760d66df01f57c47c1
Olia Rosiak <olia.rosiak@gmail.com>
2022-01-27 23:14:56 +0200

    update 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryList = document.querySelector('div.gallery');
+
+const galleryCards = galleryItems.map(createGalleryCard).join('');
+galleryList.insertAdjacentHTML('afterbegin', galleryCards);
+
+function createGalleryCard({ description, preview, original }) { 
+    return `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `
+};
+
+let gallerySlider = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, });
