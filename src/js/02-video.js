7b59317ae96e2fad92bd582eab70205ff9c23ab0
Olia Rosiak <olia.rosiak@gmail.com>
2022-01-28 02:12:02 +0200

    update 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
+// або import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const playerTimeUpdate = function (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}
+
+player.on('timeupdate', throttle(playerTimeUpdate, 1000));
+
+const videoStopTime = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(videoStopTime);
+
+
+
+//В документації приклад такий був:
+// player.setCurrentTime(videoStopTime).then(function(seconds) {
+//     console.log(seconds);    
+// }).catch(function (error) {
+//     console.log(error.name);
+// });
